a95c21959f44f81db77cbe267564f1793a2a72cb
“msamy250” <m.samyshaban@gmail.com>
2023-08-12 09:53:12 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+puts('"Programming is like building a multilingual puzzle');
 
-return (puts);
+return (0);
 
 }
