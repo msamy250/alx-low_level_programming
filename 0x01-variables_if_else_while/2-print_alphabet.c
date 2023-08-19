b32b6aad4313c8e96850c6aaa80c452ecbaa85be
Mahmoud Shaban <Mahmoud.Shaban@MacbookPros-MacBook-Pro.local>
2023-08-19 18:07:40 +0300

    update

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/*betty style doc for function main goes there */
+/**
+ *main - print prints the alphabet in lowercase.
+ *Return: 0
+ */
+int main(void)
+{
+	char letter
+	for (letter = "A"; letter => "z"; letter++)
+	putchar(letter);
+	putchar(\n)
+
+	return (0);
+}
