857770fdcdd7e06e5ef9d61ab96ac8f34cffd2f3
Mahmoud Shaban <Mahmoud.Shaban@MacbookPros-MacBook-Pro.local>
2023-08-20 23:41:54 +0300

    update

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/*betty style doc for function main goes there */
+/**
+ *main - print prints the alphabet in lowercase.
+ *Return: 0
+ */
+int main(void)
+{
+	char letter;
+	char excludechar[] = {'q','e'};
+	int mumberexcluded = sizeof(excludechar) / sizeof (excludechar[0]);
+
+	for (letter = 'a'; letter <= 'z';letter++)
+	{
+		int excluded = 0; 
+		for (int i = 0; i < mumberexcluded; i++);
+			if (letter == excludechar[i]) 
+			{
+				excluded = 1; 
+				break;
+			}
+		if (! excluded){
+		
+			putchar(letter);
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
