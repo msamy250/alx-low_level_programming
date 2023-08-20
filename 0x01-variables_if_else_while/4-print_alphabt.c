355be9e0e8fbfbd2da20f09642f6ad4569a06d79
Mahmoud Shaban <Mahmoud.Shaban@MacbookPros-MacBook-Pro.local>
2023-08-20 23:47:01 +0300

    update

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,24 +8,28 @@
 int main(void)
 {
 	char letter;
-	char excludechar[] = {'q','e'};
-	int mumberexcluded = sizeof(excludechar) / sizeof (excludechar[0]);
+	char excludechar[] = { 'q', 'e' };
 
-	for (letter = 'a'; letter <= 'z';letter++)
+	int mumberexcluded = sizeof(excludechar) / sizeof(excludechar[0]);
+
+	for (letter = 'a'; letter <= 'z'; letter++)
 	{
-		int excluded = 0; 
-		for (int i = 0; i < mumberexcluded; i++);
-			if (letter == excludechar[i]) 
-			{
-				excluded = 1; 
-				break;
-			}
-		if (! excluded){
-		
+		int excluded = 0;
+
+		for (int i = 0; i < mumberexcluded; i++)
+		{
+
+		if (letter == excludechar[i])
+		{
+			excluded = 1;
+			break;
+		}
+
+		if (!excluded)
+		{
 			putchar(letter);
 		}
-	}
-
 	putchar('\n');
+		}
 	return (0);
 }
