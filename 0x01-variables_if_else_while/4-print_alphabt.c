e838d9b97e30cbbb78603f191b6effdefde8a9a2
Mahmoud Shaban <Mahmoud.Shaban@MacbookPros-MacBook-Pro.local>
2023-08-21 01:42:42 +0300

    update

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,10 +10,15 @@ int main(void)
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
-		{
-			if (!(letter=='e'&& letter=='q'))
-				putchar(letter);
-		}
+	{
+		if (letter == 'e')
+			continue;
+		else if (letter == 'q')
+			continue;
+
+		putchar(letter);
+	}
+
 	putchar('\n');
 
 	return (0);
