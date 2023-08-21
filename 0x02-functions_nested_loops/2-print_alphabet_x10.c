e78e563706e878c0a32f47d161da32157229977e
Mahmoud Shaban <Mahmoud.Shaban@MacbookPros-MacBook-Pro.local>
2023-08-22 01:00:02 +0300

    update

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - prints alphabet, in lowercase,
+ *10 times followed by a new line.
+ *Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char letter;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+
+		_putchar('\n');
+	}
+}
