9a552907c1ec54c5ab4950bb05fe340ccc2ade15
Mahmoud Shaban <Mahmoud.Shaban@MacbookPros-MacBook-Pro.local>
2023-08-27 20:18:57 +0300

    update

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *print_diagonal - draws a straight line
+ *@n: the number of times charchter will print
+ */
+void print_diagonal(int n)
+{
+	int i;
+	int s;
+
+	if (n > 0)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (s = 1; s <= i; s++)
+				_putchar(' ');
+		_putchar(92);
+		_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
+
