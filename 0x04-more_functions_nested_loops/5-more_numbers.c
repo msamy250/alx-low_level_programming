b86f071a493c1b1b4387faa49c5391a6484b81bb
Mahmoud Shaban <Mahmoud.Shaban@MacbookPros-MacBook-Pro.local>
2023-08-27 18:14:50 +0300

    update

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,18 +8,18 @@ void more_numbers(void)
 	int i;
 	int c;
 
-	for (c = 0; c <= 9; c++)
+	for (c = 1; c <= 10; c++)
 	{
 		for (i = 0; i <= 14; i++)
 		{
-			if (i < 10)
-			{
-				_putchar(i + '0');
-			}
-			else
+			if (i > 10)
 			{
 				_putchar(i / 10 + '0');
 				_putchar(i % 10 + '0');
+			}
+			else
+			{
+				_putchar(i + '0');
 			}
 		}
 	_putchar('\n');
