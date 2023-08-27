b3aff6833dc157ed255121330bda600a140bc568
Mahmoud Shaban <Mahmoud.Shaban@MacbookPros-MacBook-Pro.local>
2023-08-27 20:29:50 +0300

    update

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,9 +11,9 @@ void print_diagonal(int n)
 
 	if (n > 0)
 	{
-		for (i = 1; i <= n; i++)
+		for (i = 0; i <= n; i++)
 		{
-			for (s = 1; s <= i; s++)
+			for (s = 0; s <= i; s++)
 			{
 				_putchar(' ');
 			}
