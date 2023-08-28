fc04ba96f40fb466ea644efe98da765ec16858fe
Mahmoud Shaban <Mahmoud.Shaban@MacbookPros-MacBook-Pro.local>
2023-08-29 02:55:36 +0300

    update

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *_strlen - returns the length of a string
+ *@s: is char
+ *Return: 0
+ */
+int _strlen(char *s)
+{
+	int l;
+
+	for (l = 0; l <= s[l]; l++)
+	return (l);
+}
