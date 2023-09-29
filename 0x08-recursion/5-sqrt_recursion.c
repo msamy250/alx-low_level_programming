7aa097ac6111374bab7c00b2903db64c526343ff
Mahmoud Shaban <Mahmoud.Shaban@MacbookPros-MacBook-Pro.local>
2023-09-29 03:29:35 +0300

    update

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *_sqrt_recursion - function that returns
+ *the natural square root of a number.
+ *value - function to calculate
+ *@x: value
+ *@n: number
+ *Return: 0
+ */
+int value(int n, int x);
+
+int _sqrt_recursion(int n)
+{
+	return (value(n, 1));
+}
+/*value - function to calculate*/
+int value(int n, int x)
+{
+	if (x * x < n)
+		return (value(n, x + 1));
+}
+
+else
+{
+	return (x);
+}
