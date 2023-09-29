3eb35d05467c091b93f728cacbb13fab8694dabc
Mahmoud Shaban <Mahmoud.Shaban@MacbookPros-MacBook-Pro.local>
2023-09-29 03:11:43 +0300

    update

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- *_pow_recursion - function that returns the value of x raised to the power of y
+ *_pow_recursion - function that returns
+ *the value of x raised to the power of y
  *@x: number
  *@y: number
  *Return: 0
@@ -12,6 +13,11 @@ int _pow_recursion(int x, int y)
 	{
 		return (-1);
 	}
-    
+	else if (y == 1)
+		return (x);
+
+	else if (y == 0)
+		return (1);
+
 	return (x * _pow_recursion(x, y - 1));
 }
