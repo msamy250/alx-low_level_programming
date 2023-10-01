ce43b1673c1abcf3347db2f90ff114f677d1ca1b
Mahmoud Shaban <Mahmoud.Shaban@MacbookPros-MacBook-Pro.local>
2023-10-01 18:55:16 +0300

    update

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -6,31 +6,31 @@
  *@x: the checker
  *Return: 1 if the number  is  prime
  */
-int prime_checker (int n, int x);
-int prime_checker (int n, int x)
+int prime_checker(int n, int x);
+int prime_checker(int n, int x)
 {
 		if (n <= 1)
 	{
 		return (0);
 	}
-	else if ( n == 2)
+	else if (n == 2)
 	{
 		return (1);
 	}
 	else if (n % x == 0)
 	{
-		return(0);
+		return (0);
 	}
-	else if (x >= n/2)
+	else if (x >= n / 2)
 	{
-		return(1);
+		return (1);
 	}
 	else
 	{
-		return (prime_checker(n, x +1));
+		return (prime_checker(n, x + 1));
 	}
 }
 int is_prime_number(int n)
 {
-	return(prime_checker(n, n-1));
+	return (prime_checker(n, n - 1));
 }
