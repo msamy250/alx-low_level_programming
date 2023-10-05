16ecd2d8cf9a29c849b5e65b1277d333d13c4cde
Mahmoud Shaban <Mahmoud.Shaban@MacbookPros-MacBook-Pro.local>
2023-10-06 01:31:15 +0300

    update

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,9 +9,9 @@
 char *create_array(unsigned int size, char c)
 {
 	char *array;
-    unsigned int i;
+	unsigned int i;
 
-    if (size == 0)
+	if (size == 0)
 	{
 		return (NULL);
 	}
@@ -22,9 +22,10 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	}
 
-    for (i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		array[i] = c;
 	}
-    return (array);
+
+	return (array);
 }
