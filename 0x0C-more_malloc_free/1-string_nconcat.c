021fd0ec7ab21c6a538cd7d1910cd518b1020ffa
Mahmoud Shaban <Mahmoud.Shaban@MacbookPros-MacBook-Pro.local>
2023-10-10 02:37:21 +0300

    update

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *string_nconcat - function that concatenates two strings
+ *@s1: string
+ *@s2: string 2
+ *@n: bytes
+ *Return: 0
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *s1 = (char *) malloc(n * sizeof(10))
+
+	if (ptr == NULL)
+	{
+		return (NULL)
+	}
+    
+}
