a0f660dd44d09c59b717e56266357b7e7a8b0fc0
Mahmoud Shaban <Mahmoud.Shaban@MacbookPros-MacBook-Pro.local>
2023-10-10 02:40:19 +0300

    update

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,8 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
+
+
+#endif
