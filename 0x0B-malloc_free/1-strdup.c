a6222c6d66146e0178f484096b7f62b1df48f185
Mahmoud Shaban <Mahmoud.Shaban@MacbookPros-MacBook-Pro.local>
2023-10-10 17:02:56 +0300

    update

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,31 +1,32 @@
 #include "main.h"
 
 /**
- *_strdup - duplicate  string
- *@str: string 1 
- *@str2: new string duplicated
+ *_strdup - duplicate string
+ *@str: string 1
+ *@str2: new string
  *Return: pointer of  the duplicated string
  */
 char *_strdup(char *str)
 {
 	int length = 0;
-    char *str2;
-    int i; 
+	char *str2;
+	int i;
 
-    while (str[length]!= '\0')
-    {
-        length++;
-    }
-    str2 = (char *) malloc (length + 1);
-    if (str2 ==  NULL)
-    {
-        return(NULL);
-    }
+	while (str[length] != '\0')
+	{
+		length++;
+	}
 
-    for (i = 0; i < length; i++)
-    {
-        str2[i] = str[i];
-    }
+	str2 = (char *) malloc(length + 1);
+	if (str2 == NULL)
+	{
+		return (NULL);
+	}
 
-    return (str2);
+	for (i = 0; i < length; i++)
+	{
+		str2[i] = str[i];
+	}
+    str2[length] = '\0';
+	return (str2);
 }
