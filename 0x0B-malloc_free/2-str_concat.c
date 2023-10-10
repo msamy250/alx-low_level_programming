edb8d385303c008fee3f7b77bf2953e0e0f13bd0
Mahmoud Shaban <Mahmoud.Shaban@MacbookPros-MacBook-Pro.local>
2023-10-10 18:48:34 +0300

    update

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,54 @@
+#include "main.h"
+
+/**
+ *str_concat - concatenates strings
+ *@str: string 1
+ *Return: pointer of  the duplicated string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int c1 = 0;
+    int c2 = 0;
+    int length;
+    char *ptr;
+    int i = 0;
+
+    if (s1 == NULL)
+    {
+        s1 = "";
+    }
+ if (s2 == NULL)
+    {
+        s2 = "";
+    }
+	while (s1[c1] != '\0')
+    {
+        c1++;
+    }
+    while (s1[c2] != '\0')
+    {
+        c2++;
+    }
+
+    length = c1 + c2;
+
+    ptr = (char *)malloc (length + 1);
+    if (ptr == NULL)
+		{
+			return (NULL);
+		}
+
+
+    for (i = 0; i < c1; i++)
+    {
+        ptr[i] = s1[i];
+    }
+     for (i = 0; i < c2; i++)
+    {
+        ptr[c1 + i] = s2[i];
+    }
+
+    ptr[length] = '\0';
+    
+    return (ptr);
+}
