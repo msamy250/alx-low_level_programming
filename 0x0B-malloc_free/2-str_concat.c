11dbb81b94ae9ef3470398ac0bdbaad1819326e5
Mahmoud Shaban <Mahmoud.Shaban@MacbookPros-MacBook-Pro.local>
2023-10-10 19:00:43 +0300

    update

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,7 +10,6 @@ char *str_concat(char *s1, char *s2)
 {
 	int c1 = 0;
 	int c2 = 0;
-	int length;
 	char *ptr;
 	int i = 0;
 
@@ -18,41 +17,33 @@ char *str_concat(char *s1, char *s2)
 	{
 		s1 = "";
 	}
-
 	if (s2 == NULL)
 	{
 		s2 = "";
 	}
-
 	while (s1[c1] != '\0')
 	{
 		c1++;
 	}
-
 	while (s2[c2] != '\0')
 	{
 		c2++;
 	}
+	ptr = (char *) malloc(c1 + c2 + 1);
 
-	length = c1 + c2;
-
-	ptr = (char *) malloc(length + 1);
 	if (ptr == NULL)
 	{
 		return (NULL);
 	}
-
 	for (i = 0; i < c1; i++)
 	{
 		ptr[i] = s1[i];
 	}
-
 	for (i = 0; i < c2; i++)
 	{
 		ptr[c1 + i] = s2[i];
 	}
-
-	ptr[length] = '\0';
+	ptr[c1 + c2] = '\0';
 
 	return (ptr);
 }
