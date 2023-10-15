b1c0703ec5502424f8ea3ab69c27011fa5bf0109
Mahmoud Shaban <Mahmoud.Shaban@MacbookPros-MacBook-Pro.local>
2023-10-15 22:40:35 +0300

    update

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -3,19 +3,20 @@
 /*betty style doc for function main goes there */
 /**
  *main - the number of arguments passed into it.
-* @argc: count of the arguments
-* @argv: an array of pointers to the strings
+ *@argc: count of the arguments
+ *@argv: an array of pointers to the strings
+ *@i: number of loops
  *Return: 0;
  */
 int main(int argc, char *argv[])
 {
 	int i;
 
-    if (argc > 0)
+	if (argc > 0)
 	{
 		for (i = 0; i < argc; i++)
 
-        printf("%s\n", argv[i]);
+			printf("%s\n", argv[i]);
 	}
 
 	return (0);
