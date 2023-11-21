19fba3605308e57fe312e59af6c90f889593f48c
“msamy250” <m.samyshaban@gmail.com>
2023-11-22 01:12:24 +0200

    update

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ *create_array - creates an array of chars
+ *@size: size of array
+ *@c: char
+ *Return: 0
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	array = (char *) malloc(size * sizeof(char));
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		array[i] = c;
+	}
+
+	return (array);
+}
---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ *str_concat - concatenates strings
+ *@s1: string 1
+ *@s2: string 2
+ *Return: pointer
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int c1 = 0;
+	int c2 = 0;
+	char *ptr;
+	int i = 0;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	while (s1[c1] != '\0')
+	{
+		c1++;
+	}
+	while (s2[c2] != '\0')
+	{
+		c2++;
+	}
+	ptr = (char *) malloc(c1 + c2 + 1);
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < c1; i++)
+	{
+		ptr[i] = s1[i];
+	}
+	for (i = 0; i < c2; i++)
+	{
+		ptr[c1 + i] = s2[i];
+	}
+	ptr[c1 + c2] = '\0';
+
+	return (ptr);
+}
