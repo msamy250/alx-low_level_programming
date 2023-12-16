81bc41be966c24f348607f56170fd6b27dad3ced
“msamy250” <m.samyshaban@gmail.com>
2023-12-17 01:46:21 +0200

    update

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -36,11 +36,9 @@ int **alloc_grid(int width, int height)
 			while (i >= 0)
 			{
 				free(grid[i--]);
-			free(grid);
-			return (NULL);
+				free(grid);
+				return (NULL);
 			}
-
-			
 
 		for (j = 0; j < width; j++)
 		{
