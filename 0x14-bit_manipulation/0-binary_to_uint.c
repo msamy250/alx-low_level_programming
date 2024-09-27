050dec1638c187dc45d3c10eb0d7bd8bdd1ec6d9
Mahmoud Samy <mahmoudsamy@192.168.1.16>
2024-09-27 20:06:35 +0300

    update

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,43 +1,30 @@
 #include "main.h"
 
 /**
- * binary_to_uint - Converts a binary string to an
- * unsigned integer.
- * @b: String containing the binary representation.
- *
- * Return: The converted unsigned integer value.
+ * binary_to_uint - A finction that converts binary number
+ * to an unsigned integer
+ * @b: A pointer to binary string
+ * Return: unsigned integer converted number
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int ui;
-	int len, base_two;
+	unsigned int base = 1, result = 0, len = 0;
 
-	// Return 0 if input string is NULL
-	if (!b)
+	if (b == NULL)
 		return (0);
 
-	ui = 0;
+	while (b[len])
+		len++;
 
-	// Calculate the length of the binary string
-	for (len = 0; b[len] != '\0'; len++)
-		;
-
-	// Loop through the string, starting from the least significant bit
-	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
+	while (len)
 	{
-		// If any character is not '0' or '1', return 0 as it's invalid
-		if (b[len] != '0' && b[len] != '1')
-		{
+		if (b[len - 1] != '0' && b[len - 1] != '1')
 			return (0);
-		}
 
-		// Add to result if the current character is '1'
-		if (b[len] & 1)
-		{
-			ui += base_two;
-		}
+		if (b[len - 1] == '1')
+			result += base;
+		base *= 2;
+		len--;
 	}
-
-	// Return the resulting unsigned integer
-	return (ui);
+	return (result);
 }
