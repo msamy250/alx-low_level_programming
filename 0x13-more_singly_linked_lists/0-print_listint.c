422ea261bbc0488b5fe97799e12612ebbe83a567
Mahmoud Samy <mahmoudsamy@Mahmouds-MacBook-Pro.local>
2024-09-28 00:02:37 +0300

    update

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ *print_listint - function that prints all the elements of a list_t list.
+ *@h: A pointer to the head of the linked list.
+ *Return: number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	int count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ *free_listint2- function that frees a list_t list.
+ *@head: A pointer to the head of the linked list.
+ *Return: nothing
+ */
+
+void free_listint2(listint_t **head)
+{
+	if (head == NULL)
+	{
+		return;
+	}
+
+	while (*head != NULL)
+	{
+		listint_t *temp = *head;
+
+		*head = (*head)->next;
+
+		free(temp);
+	}
+}
